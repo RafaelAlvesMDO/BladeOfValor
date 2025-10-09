432991b6c857de87d848b51894d24d32701a2613
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-10-08 21:16:13 -0300

    feat: updated PlayerMovement to include side facing, jump, and fall detection

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        // Side Looking
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Animator Parameters
+        anim.SetBool("walk", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+        // Jump Trigger
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        // Fall Trigger
+        if (body.velocity.y < -0.01f && !grounded)
+            Fall();
+        }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void Fall()
+    {
+        anim.SetTrigger("fall");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
