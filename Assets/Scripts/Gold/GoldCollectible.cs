e608d4ae8a13b306adfebbd7a9b7812fbaf0d2e9
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-11-21 18:52:30 -0300

    Feat: Updated with Sound Effect when collected.

--- Assets/Scripts/Gold/GoldCollectible.cs
+++ Assets/Scripts/Gold/GoldCollectible.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class GoldCollectible : MonoBehaviour
 {
     [SerializeField] private int goldValue = 10;
+    [SerializeField] private AudioClip CoinSound;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            SoundManager.instance.PlaySound(CoinSound);
             collision.GetComponent<Gold>().AddGold(goldValue);
             gameObject.SetActive(false);
         }
