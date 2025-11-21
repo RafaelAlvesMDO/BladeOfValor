fa72d00d89487dabe042ff5b04bcf8cad8cbc04a
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-11-21 18:52:48 -0300

    Feat: Updated with Sound Effect when collected.

--- Assets/Scripts/Mana/ManaCollectible.cs
+++ Assets/Scripts/Mana/ManaCollectible.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class ManaCollectible : MonoBehaviour
 {
     [SerializeField] private float manaValue;
+    [SerializeField] private AudioClip ManaSound;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            SoundManager.instance.PlaySound(ManaSound);
             collision.GetComponent<Mana>().AddMana(manaValue);
             gameObject.SetActive(false);
         }
